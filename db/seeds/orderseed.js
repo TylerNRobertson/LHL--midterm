7a6ad6adbf7e0b4affb35bab516e15b78280ef80
Bernard Roach <bernieroach@gmail.com>
2017-10-26 12:49:23 +0000

    changes to up/down for order and customer

--- db/seeds/orderseed.js
+++ db/seeds/orderseed.js
@@ -1,18 +1,38 @@
 exports.seed = function(knex, Promise) {
-  return knex('customer').del()
-    .then(function () {
-      let rightNow = new Date();
-      return Promise.all([
-        knex('customer').insert({  id: 1,
-                                   first_name: 'Bernard',
-                                   last_name: 'Roach',
-                                   address  : 'Only the finest address',
-                                   phone    : 'BES-TPH-ONE#'
-                                 }),
-          knex('order').insert({  customerId: 1,
-                                date_time_created: rightNow.getSeconds(),
-                                date_time_pickup:  rightNow.setMinutes(rightNow.getMinutes() + 30).getSeconds(),
-                            }),
-      ]);
-    });
-};
+      let rightNow = new Date().toISOString();
+    return knex('customer').del()
+        .then(function() {
+
+            return Promise.all([
+                knex('customer').insert({
+                    id: 1,
+                    first_name: 'Someguy',
+                    last_name: 'Laughing',
+                    address: '123 Street funkytown',
+                    phone: '111-111-1111',
+                    email: 'test@test.com'
+                }),
+                knex('customer').insert({
+                    id: 2,
+                    first_name: '2Someguy2',
+                    last_name: '2Laughing',
+                    address: '222 Street funkytown',
+                    phone: '222-222-2222',
+                    email: 'test@test.com'
+                }),
+            ]).then(() => {
+              return knex('order').del()
+        .then(function() {
+                return Promise.all([
+
+                    knex('order').insert({
+                        id: 1,
+                        customerId: 2,
+                        date_time_created: rightNow
+
+                    })
+                ]);
+              });
+            });
+        });
+}
